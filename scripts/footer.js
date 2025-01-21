be55a54b4a2db2dc86cab8b58ad706c3e483c6ca
Aidan Scott <160932113+AidanJScott@users.noreply.github.com>
2025-01-21 14:38:22 -0500

    Added footer dynamic selection

---
+++ scripts/footer.js
@@ -0,0 +1,13 @@
+"use strict";
+let footerRows = document.querySelectorAll(".footer_hover");
+footerRows.forEach(row => {
+    let rowText = row.textContent
+    let altRowText = row.getAttribute("altText");
+    row.addEventListener("mouseover", event => {
+        row.textContent = altRowText
+    });
+    row.addEventListener("mouseout", event => {
+        row.textContent = rowText;
+    });
+
+});
