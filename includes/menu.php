c81e26af17f6ea82d2dfc7415090b0d84b2ad6b1
Aidan Scott <160932113+AidanJScott@users.noreply.github.com>
2025-04-08 10:10:23 -0400

    updated navigation bar to show login status

--- includes/menu.php
+++ includes/menu.php
@@ -9,6 +9,24 @@
             </ul>
         </li>
         <li id="shop"><a href="shop.php" <?php if ($currentPage == 'shop.php') {echo 'id="current"'; } ?>>SHOP</a></li>
-        <li id="join"><a href="join.php" <?php if ($currentPage == 'join.php') {echo 'id="current"'; } ?>>JOIN</a></li>
+        <?php
+		if(isset($_SESSION['first_name']) AND isset($_SESSION['email'])){
+			echo "<li><a href=\"loggedOut.php\""; 
+			if ($currentPage == 'loggedOut.php') {echo 'id="here"'; }
+			echo ">LOG OUT</a></li>";
+		}
+		else {
+            echo "<li id=\"join\">JOIN ˅";
+            echo "<ul>";
+			echo "<li><a href=\"join.php\""; 
+			if ($currentPage == 'join.php') {echo 'id="here"'; }
+			echo ">REGISTER</a></li>";
+			echo "<li><a href=\"login.php\""; 
+			if ($currentPage == 'login.php') {echo 'id="here"'; }
+			echo ">LOG IN</a></li>";
+            echo "</ul>";
+            echo "</li>";
+		}
+		?>
     </ul>
 </nav>
