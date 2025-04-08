f183f9db1e0f258df56bc4f0217f212d92b2e289
Aidan Scott <160932113+AidanJScott@users.noreply.github.com>
2025-04-08 10:14:50 -0400

    added secure and regular connections and log out page

---
+++ secure_conn.php
@@ -0,0 +1,10 @@
+<?php 
+$https = $_SERVER['HTTPS'];
+if($https != "on"){
+    $host = $_SERVER['HTTP_HOST'];
+    $uri = $_SERVER['REQUEST_URI'];
+    $url = 'https://' . $host . $uri;
+    header("Location: " . $url);
+    exit();
+}
+?>
